99e7660c3f5e1d27698875181381812c07eda4f0
sebastian <sebastiangeorge004@gmail.com>
2026-06-29 16:31:24 +0530

    add:added all imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,22 @@
 from models.user import User
+from models.appraisal import Appraisal
+from models.appraisal_lead_assignment import AppraisalLeadAssignment
+from models.lead_feedback import LeadFeedback
+from models.audit import Audit
+from models.cycles import Cycles
+from models.self_appraisal import SelfAppraisal
+from models.appraisal_summary import AppraisalSummary
+from models.competencies import Competencies
 
-__all__ = ["User"]
+
+__all__ = [
+    "User",
+    "Appraisal",
+    "AppraisalLeadAssignment",
+    "LeadFeedback",
+    "Audit",
+    "Cycles",
+    "SelfAppraisal",
+    "AppraisalSummary",
+    "Competencies",
+]
