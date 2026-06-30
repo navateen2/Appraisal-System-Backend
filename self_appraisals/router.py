64269df4200caa264844b66bb3f5038aa494f5db
Sebastian  George <sebastiangeorge004@gmail.com>
2026-06-30 11:40:41 +0530

    merging master to test (#10)
    * fix: appraisal router + schemas
    
    * fix:main.py
    
    * add:added all imports
    
    * feat: add lead feedback api
    
    * fix : corrected destructuring of role
    
    * fix:corrected statement
    
    * fix:  appraisals
    
    * fix:corrected arguments
    
    * feature: add crud endpoints for self appraisal
    
    * fix: add get_by_appraisal_id and import self appraisal router
    
    * feat: add lead assignment api
    
    * feat:complete lead assignment implementation
    
    * Feature/audit adding audit feature (#8)
    
    * add:added audit feature
    
    * merging master to feature/audit   (#5)
    
    * fix: appraisal router + schemas
    
    * fix:main.py
    
    * add:added all imports
    
    * feat: add lead feedback api
    
    * fix : corrected destructuring of role
    
    * fix:corrected statement
    
    * fix:  appraisals
    
    * fix:corrected arguments
    
    ---------
    
    Co-authored-by: navaneet <navateen2@gmail.com>
    Co-authored-by: Suhana Sulfeekker <suhanasulfeekker.mec@gmail.com>
    
    * fix:added a system id for cases whereuser idis missing
    
    * fix:positional argument mismatch
    
    * add:middleware for audit
    
    * fix: corrected audit code
    
    ---------
    
    Co-authored-by: navaneet <navateen2@gmail.com>
    Co-authored-by: Suhana Sulfeekker <suhanasulfeekker.mec@gmail.com>
    
    * refactor: change delete to soft delete
    
    * fix:added authentication for audit
    
    ---------
    
    Co-authored-by: navaneet <navateen2@gmail.com>
    Co-authored-by: Suhana Sulfeekker <suhanasulfeekker.mec@gmail.com>
    Co-authored-by: denis <denisgeorgework@gmail.com>

--- self_appraisals/router.py
+++ self_appraisals/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from auth.schemas import TokenPayload
 from database.connection import get_db
 from self_appraisals.schemas import SelfAppraisalCreate, SelfAppraisalResponse, SelfAppraisalUpdate
+from auth.dependencies import get_current_user
 from . import service
 
 router = APIRouter(
@@ -9,24 +11,60 @@ router = APIRouter(
     tags=["Self Appraisal"],
 )
 
-@router.post("/",response_model=SelfAppraisalResponse,status_code=status.HTTP_201_CREATED,)
-async def create_self_appraisal(payload: SelfAppraisalCreate,db: AsyncSession = Depends(get_db),):
+
+@router.post(
+    "/",
+    response_model=SelfAppraisalResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_self_appraisal(
+    payload: SelfAppraisalCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: TokenPayload = Depends(get_current_user),
+):
     return await service.create_self_appraisal(db, payload)
 
-@router.get("/",response_model=list[SelfAppraisalResponse],)
-async def get_all_self_appraisals(db: AsyncSession = Depends(get_db),):
+
+@router.get(
+    "/",
+    response_model=list[SelfAppraisalResponse],
+)
+async def get_all_self_appraisals(
+    db: AsyncSession = Depends(get_db),
+):
     return await service.get_all_self_appraisals(db)
 
-@router.get("/{self_appraisal_id}",response_model=SelfAppraisalResponse,)
-async def get_self_appraisals(self_appraisal_id: int, db: AsyncSession = Depends(get_db),):
+
+@router.get(
+    "/{self_appraisal_id}",
+    response_model=SelfAppraisalResponse,
+)
+async def get_self_appraisals(
+    self_appraisal_id: int,
+    db: AsyncSession = Depends(get_db),
+):
     return await service.get_self_appraisal_by_id(db, self_appraisal_id)
 
-@router.put("/{self_appraisal_id}",response_model=SelfAppraisalResponse,)
-async def update_self_appraisals(self_appraisal_id: int, payload:SelfAppraisalUpdate, db: AsyncSession = Depends(get_db),):
-    return await service.update_self_appraisal(db, self_appraisal_id, payload)    
 
-@router.delete("/{self_appraisal_id}", status_code=status.HTTP_204_NO_CONTENT,)
-async def delete_self_apraisals(self_appraisal_id: int,db: AsyncSession = Depends(get_db),):
+@router.put(
+    "/{self_appraisal_id}",
+    response_model=SelfAppraisalResponse,
+)
+async def update_self_appraisals(
+    self_appraisal_id: int,
+    payload: SelfAppraisalUpdate,
+    db: AsyncSession = Depends(get_db),
+    current_user: TokenPayload = Depends(get_current_user),
+):
+    return await service.update_self_appraisal(db, self_appraisal_id, payload)
+
+
+@router.delete(
+    "/{self_appraisal_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_self_apraisals(
+    self_appraisal_id: int, db: AsyncSession = Depends(get_db), current_user: TokenPayload = Depends(get_current_user)
+):
     await service.delete_self_appraisal(db, self_appraisal_id)
-    return None   
-
+    return None
