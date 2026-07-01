18ef8fca39ab19fd2c52c2dfd8b3a75ea3d6ee8e
sebastian <sebastiangeorge004@gmail.com>
2026-07-01 23:57:52 +0530

    fix: remove deletad appraisals

--- appraisals/repo.py
+++ appraisals/repo.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import select
 from sqlalchemy.orm import joinedload, selectinload
-from models.appraisal import Appraisal,AppraisalStatus
+from models.appraisal import Appraisal, AppraisalStatus
 from exceptions import ConflictException
 
 
@@ -16,12 +16,8 @@ async def create(
     cycle_id: int,
     employee_id: int,
 ) -> Appraisal:
-    appraisal = Appraisal(
-        cycle_id=cycle_id,
-        employee_id=employee_id,
-        status=AppraisalStatus.Initiated
-        )
-    
+    appraisal = Appraisal(cycle_id=cycle_id, employee_id=employee_id, status=AppraisalStatus.Initiated)
+
     db.add(appraisal)
     try:
         await db.commit()
@@ -33,7 +29,7 @@ async def create(
     return appraisal
 
 
-async def get_all_appraisals(db: AsyncSession, status: str|None):
+async def get_all_appraisals(db: AsyncSession, status: str | None):
     stmt = select(Appraisal).where(Appraisal.deleted_at.is_(None))
     if status:
         stmt = stmt.where(Appraisal.status == status)
@@ -65,16 +61,16 @@ async def get_appraisal_by_id(appraisal_id: int, db: AsyncSession):
 #     stmt = select(Appraisal).where(Appraisal.email == email, Appraisal.deleted_at.is_(None))
 #     user = await db.scalars(stmt)
 #     return user.first()
- 
 
 
 async def soft_delete_appraisal(appraisal: Appraisal, db: AsyncSession):
     await db.commit()
     return
 
+
 async def update_appraisal_idp(db: AsyncSession, appraisal: Appraisal, idp_text: str) -> Appraisal:
     appraisal.idp_text = idp_text
-    
+
     await db.commit()
     await db.refresh(appraisal)
     return appraisal
@@ -82,19 +78,21 @@ async def update_appraisal_idp(db: AsyncSession, appraisal: Appraisal, idp_text:
 
 async def update_appraisal_hr_notes(db: AsyncSession, appraisal: Appraisal, hr_notes: str) -> Appraisal:
     appraisal.hr_notes = hr_notes
-    
+
     await db.commit()
     await db.refresh(appraisal)
     return appraisal
+
 
 async def get_employee_appraisal_history(db: AsyncSession, employee_id: int) -> List[Appraisal]:
     stmt = (
         select(Appraisal)
         .options(joinedload(Appraisal.cycle))
-        .filter(Appraisal.employee_id == employee_id)
+        .filter(Appraisal.employee_id == employee_id, Appraisal.deleted_at is None)
     )
     result = await db.execute(stmt)
     return list(result.scalars().all())
+
 
 async def update_appraisal_status(db: AsyncSession, appraisal: Appraisal, status: AppraisalStatus) -> Appraisal:
     appraisal.status = status
@@ -102,13 +100,11 @@ async def update_appraisal_status(db: AsyncSession, appraisal: Appraisal, status
     await db.refresh(appraisal)
     return appraisal
 
+
 async def get_detailed_appraisal_by_id(db: AsyncSession, appraisal_id: int) -> Appraisal | None:
     stmt = (
         select(Appraisal)
-        .options(
-            selectinload(Appraisal.self_appraisal),
-            selectinload(Appraisal.lead_assignments)
-        )
+        .options(selectinload(Appraisal.self_appraisal), selectinload(Appraisal.lead_assignments))
         .filter(Appraisal.id == appraisal_id)
     )
     result = await db.execute(stmt)
