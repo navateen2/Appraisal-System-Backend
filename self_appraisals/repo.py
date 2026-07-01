5b06827aa48ef8cb0aaa962619734a425c3c6c84
sebastian <sebastiangeorge004@gmail.com>
2026-07-01 23:23:34 +0530

    fix:corrected get self appraisal_by id

--- self_appraisals/repo.py
+++ self_appraisals/repo.py
@@ -19,7 +19,7 @@ async def get_all(db: AsyncSession):
 
 
 async def get_by_id(db: AsyncSession, self_appraisal_id: int):
-    stmt = select(SelfAppraisal).where(SelfAppraisal.id == self_appraisal_id)
+    stmt = select(SelfAppraisal).where(SelfAppraisal.appraisal_id == self_appraisal_id)
     result = await db.scalars(stmt)
     return result.first()
 
@@ -34,9 +34,8 @@ async def soft_delete_self_appraisal(db: AsyncSession, self_appraisal: SelfAppra
     await db.commit()
     return
 
+
 async def get_by_appraisal_id(db: AsyncSession, appraisal_id: int):
-    stmt = select(SelfAppraisal).where(
-        SelfAppraisal.appraisal_id == appraisal_id
-    )
+    stmt = select(SelfAppraisal).where(SelfAppraisal.appraisal_id == appraisal_id)
     result = await db.scalars(stmt)
     return result.first()
